5df44c50f988fae6a0365e8df205d812180b05e6
Stefan Poensgen <mail@stefanpoensgen.de>
2022-09-25 10:54:18 +0200

    feat: added media to order comment

---
+++ src/Subscriber/MediaFileExtensionWhitelistSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SptecOrderComments\Subscriber;
+
+use Shopware\Core\Content\Media\Event\MediaFileExtensionWhitelistEvent;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class MediaFileExtensionWhitelistSubscriber implements EventSubscriberInterface
+{
+    private SystemConfigService $systemConfigService;
+
+    public function __construct(SystemConfigService $systemConfigService)
+    {
+        $this->systemConfigService = $systemConfigService;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            MediaFileExtensionWhitelistEvent::class => 'addEntriesToFileExtensionWhitelist',
+        ];
+    }
+
+    public function addEntriesToFileExtensionWhitelist(MediaFileExtensionWhitelistEvent $event): void
+    {
+        $extensionWhitelist = $this->systemConfigService->get('SptecOrderComments.config.extensionWhitelist');
+        if (!\is_string($extensionWhitelist)) {
+            return;
+        }
+
+        $extensionWhitelist = \array_map('trim', \explode(',', $extensionWhitelist));
+        $whiteList = $event->getWhitelist();
+        $event->setWhitelist(\array_merge($whiteList, $extensionWhitelist));
+    }
+}
